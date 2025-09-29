8736753e80d75956fbc90fd8740f23742f78b094
looks32 <wheodnjs2@naver.com>
2025-09-29 15:03:34 +0900

    [feat] input, 유저 카드 컴포넌트 생성

--- src/components/Qcard.jsx
+++ src/components/Qcard.jsx
@@ -1,5 +1,41 @@
-function Qcard() {
-  return <div>Qcard</div>;
+import styled from 'styled-components';
+import messages from '../assets/ico_messages.png';
+
+const Card = styled.div`
+  padding: 20px;
+  border-radius: 16px;
+  border: 1px solid #818181;
+
+  > strong {
+    display: block;
+    margin-top: 12px;
+    font-size: 20px;
+  }
+
+  .question_area {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 28px;
+    color: #818181;
+    .title {
+      display: inline-block;
+      padding-left: 22px;
+      background: url(${messages}) no-repeat left center / 18px;
+    }
+  }
+`;
+
+function Qcard({ profile, nickName, question }) {
+  return (
+    <Card>
+      <div className="profile">{profile}</div>
+      <strong>{nickName}</strong>
+      <div className="question_area">
+        <span className="title">받은 질문</span>
+        <span>{question}개</span>
+      </div>
+    </Card>
+  );
 }
 
 export default Qcard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,15 @@
+import Input from '../components/Input';
+import Qcard from '../components/Qcard';
+import Reaction from '../components/Reaction';
+
 function Home() {
-  return <div>home</div>;
+  return (
+    <div>
+      <Input value="아초" placeholder="이름을 입력하세요" />
+      <Qcard profile="사진" nickName="아초는고양이" question="9" />
+      <Reaction />
+    </div>
+  );
 }
 
 export default Home;
