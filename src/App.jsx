c369642e54d2f2414ecbafd5f0b3da86b7b96f69
tkp12345 <tkp1@hanmail.com>
2025-09-30 21:59:20 +0900

    feat: 빨리

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import List from './pages/List';
 import Post from './pages/Post';
-import Answer from './pages/Answer';
 import NotFound from './pages/NotFound';
+import NotFound2 from './pages/NotFound';
+import Answer from './pages/Answer';
+import Answer from './pages/Answer';
 
 function App() {
   return (
     <Routes>
+      <Route path="/list" element={<List />}></Route>
       <Route path="/" element={<Home2 />}></Route>
       <Route path="/list" element={<List />}></Route>
       <Route path="/post/:id" element={<Post />}></Route>
